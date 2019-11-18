fe9ace1163d4aa1313f3b386031ea6f8d68590f5
Michael Oppitz <michael@shapediver.com>
2019-11-18 17:30:58 +0100

    different bundling approaches

--- out/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const SettingsConversion_1 = require("./SettingsConversion");
-exports.SettingsConversion = SettingsConversion_1.SettingsConversion;
-const Settings_1 = require("./versions/1.0/Settings");
-exports.Settings_1_0 = Settings_1.Settings;
-const Settings_2 = require("./versions/2.0/Settings");
-exports.Settings_2_0 = Settings_2.Settings;
