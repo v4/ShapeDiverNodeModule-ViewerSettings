06a6ba9f6ff5a7628459fd264d74adce0e36ae3a
luka <luka@shapediver.com>
2019-11-18 18:03:25 +0100

    - compilation fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
+const path = require('path');
+
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./src/main.ts",
-    output: {
-      filename: "bundle.js"
-    },
-    resolve: {
-      // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: [".ts", ".tsx", ".js"]
-    },
-    module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: "ts-loader" }
-      ]
-    }
-  };
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
