90494d5e5f4e64069d0c692db8f6bcaac6c35b46
luka <luka@shapediver.com>
2019-11-20 15:28:43 +0100

    - add node resolve

--- bundle.cjs.js
+++ bundle.cjs.js
@@ -2,8 +2,6 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var tslib = require('tslib');
-
 var SettingsVersion = /** @class */ (function () {
     // #endregion Properties (1)
     // #region Constructors (1)
@@ -50,6 +48,35 @@ var SettingsVersion = /** @class */ (function () {
     };
     return SettingsVersion;
 }());
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
+var extendStatics = function(d, b) {
+    extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return extendStatics(d, b);
+};
+
+function __extends(d, b) {
+    extendStatics(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
 
 var Setting = /** @class */ (function () {
     // #region Constructors (1)
@@ -254,7 +281,7 @@ var BaseSettings = /** @class */ (function () {
 }());
 
 var Settings = /** @class */ (function (_super) {
-    tslib.__extends(Settings, _super);
+    __extends(Settings, _super);
     // #endregion Properties (1)
     // #region Constructors (1)
     /**
@@ -339,7 +366,7 @@ var Settings = /** @class */ (function (_super) {
 
 //import typeChecks from "shapedivernodemodule-typechecks";
 var Settings$1 = /** @class */ (function (_super) {
-    tslib.__extends(Settings$1, _super);
+    __extends(Settings$1, _super);
     // #endregion Properties (1)
     // #region Constructors (1)
     /**
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
-import typescript from 'rollup-plugin-typescript3';
+import typescript from "rollup-plugin-typescript3";
+import nodeResolve from "rollup-plugin-node-resolve";
 import pkg from "./package.json";
+
 export default {
   input: "src/main.ts",
-  entry: 'src/main.ts',
-  output: [ // check output formats https://rollupjs.org/guide/en/
+  entry: "src/main.ts",
+  output: [
+    // check output formats https://rollupjs.org/guide/en/
     // commonjs for node
     {
       file: pkg.node,
@@ -12,17 +15,16 @@ export default {
     // iife, browser bundle to be included via <script> tag
     {
       file: pkg.module,
-      format: "iife"
+      format: "iife",
+      name: "ShapediverViewerSettings"
     },
     // umd, suitable for browser and node
     {
-        file: pkg.main,
-        format: "umd",
-        name: "ShapediverViewerSettings"
+      file: pkg.main,
+      format: "umd",
+      name: "ShapediverViewerSettings"
     }
   ],
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  plugins: [
-    typescript()
-  ]
+  plugins: [nodeResolve(), typescript()]
 };
